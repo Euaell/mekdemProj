6c3cea89c6bbcb21fce9f3e87631fd391d5bed36
Euael Eshete <euaelesh@outlook.com>
2021-12-16 21:54:00 +0300

    DONE

--- routes/studentService/dormitory/application.js
+++ routes/studentService/dormitory/application.js
@@ -5,17 +5,20 @@ const connection = require("C:/Users/euael/OneDrive/Desktop/Software Eng/PROJECT
 const application = express.Router();
 application.route("/")
 .get(authentication.isStudentLoggedIn, (req , res)=> {
-    res.render('Dormitory/Application');
+    res.render('Dormitory/Application', {msg: undefined});
 })
 .post(authentication.isStudentLoggedIn ,(req , res)=> {
     let adr = req.userData.StudentID;
     let bdr1 = req.body.dorm1;
     let bdr2 = req.body.dorm2;
     let bdr3 = req.body.dorm3;
-    let sql = `INSERT INTO dormitoryreq (StuduentD, preferedComp1 , preferedComp2, preferedComp3) VALUES ("${req.userData.StudentID}", "${bdr1}", "${bdr2}", "${bdr3}")`;
+    let sql = `UPDATE dormitoryreq 
+    SET preferedComp1 = "${bdr1}", preferedComp2 = "${bdr2}", preferedComp3= "${bdr3}"
+    WHERE StuduentD = "${adr}"`;
     connection.query(sql , (error , result) => {
         if (error) {
-            return console.error("error: " + error.message);
+            console.error("error: " + error.message);
+            res.render("/dormitory/Application", {msg: 1});
         }
         res.redirect("/dormitory/placement");
      });
